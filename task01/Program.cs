8ef39f14a3d484b1b2ad3966faa8cf04e629c510
ваше_имя <grigorenko-marko@rambler.ru>
2023-04-20 22:10:31 +0300

    task02

---
+++ task01/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+//N = 5 -> "1, 2, 3, 4, 5"
+//N = 6 -> "1, 2, 3, 4, 5, 6"
+
+int GetN ()
+{
+    System.Console.WriteLine("Введите N ");
+    return int.Parse(Console.ReadLine());
+}
+
+int N = GetN();
+System.Console.WriteLine();
+void Numbers(int N)
+{
+    if(N >0 ) 
+    {
+        Numbers(N-1);
+        System.Console.WriteLine(N);
+    }
+}
+
+Numbers(N);
